bf63c8bad0758c532ca9074fed3aac08e1e50ede
monydara <mdarait@gmail.com>
2016-04-22 14:44:54 +0700

    update more on room status

--- Source Code/app/models/room_master.rb
+++ Source Code/app/models/room_master.rb
@@ -1,4 +1,4 @@
 class RoomMaster < ActiveRecord::Base
-	belongs_to :status	
-	enum status:{ avaliable: 1 , busy:2 , dirty:3 }
+	belongs_to :status	, foreign_key:"status_id"
+	enum status:{ available: 1 , busy:2 , dirty:3 }
 end
