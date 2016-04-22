14d2643d6e401b1d27d2f49abc9c2f8ca54ac0aa
davuththan <davuththan@gmail.com>
2016-04-22 15:17:22 +0700

    fd

--- Source Code/app/models/room_master.rb
+++ Source Code/app/models/room_master.rb
@@ -1,5 +1,8 @@
 class RoomMaster < ActiveRecord::Base
+
 	belongs_to :floor, foreign_key: "floor_id"
 	belongs_to :category_master, foreign_key: "category_id"
-	belongs_to :status, foreign_key: "status_id"
+	belongs_to :status	, foreign_key:"status_id"
+	enum status:{ available: 1 , busy:2 , dirty:3 }
+
 end
